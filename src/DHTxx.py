666fe237e84c214ea4a4a9fa70336e515461802d
DarrenWainwright <DWainwright@buckland.com>
2020-03-03 20:26:34 -0500

    Starting adding in methods to get arguments.

--- src/DHTxx.py
+++ src/DHTxx.py
@@ -1,5 +1,20 @@
-## This script is for the temp/humidity sensor type DHTxx (11, 22 etc)
-## Written by Darren Wainwright
-## Mother Cluckers Coop Control
-## 
-import time
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Module monitors temperature and humidity.
+   Results are published to the eventUrl, eventType arguments
+
+   Part of the Mother Cluckers Coop Control project
+"""
+import argparse, time
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--event-url", "-u", help="Sets the event grid topic url")
+parser.add_argument("--event-type", "-t", help="Sets the event type [poll,auto,temp,humidity]")
+
+args = parser.parse_args()
+
+
+
+print("argList: %s" % args)
